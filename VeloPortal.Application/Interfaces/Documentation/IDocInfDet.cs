fa2a33420653d75d8bb2cb8bb6c00f19c7c9aa7c
mdrakibkhan123 <rakib.khan@itechvelocity.com>
2025-12-22 17:52:12 +0600

    merged code

---
+++ VeloPortal.Application/Interfaces/Documentation/IDocInfDet.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using VeloPortal.Domain.Entities.Documentation;
+
+namespace VeloPortal.Application.Interfaces.Documentation
+{
+    public interface IDocInfDet
+    {
+        Task<bool> UploadDocument(List<DocInfDet> docInfDets);
+    }
+}
---
+++ VeloPortal.WebApi/Controllers/V1/Documentation/FileController.cs
@@ -0,0 +1,42 @@
+﻿using Asp.Versioning;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using VeloPortal.Infrastructure.Service;
+
+namespace VeloPortal.WebApi.Controllers.V1.Documentation
+{
+    [ApiVersion("1.0")]
+    [ApiController]
+    [Route("api/v{version:apiVersion}/[controller]")]
+    [Authorize]
+    public class FileController : ControllerBase
+    {
+        private readonly IFtpService _ftpService;
+
+        public FileController(IFtpService ftpService)
+        {
+            _ftpService = ftpService;
+        }
+
+        [HttpPost("upload")]
+        public async Task<IActionResult> Upload(IFormFile file)
+        {
+            var url = await _ftpService.UploadAsync(file, "service-requests");
+            return Ok(new { url });
+        }
+
+        [HttpGet("download")]
+        public async Task<IActionResult> Download(string fileUrl)
+        {
+            var base64 = await _ftpService.DownloadAsBase64Async(fileUrl);
+            return Ok(base64);
+        }
+
+        [HttpDelete("delete")]
+        public async Task<IActionResult> Delete(string fileUrl)
+        {
+            await _ftpService.DeleteAsync(fileUrl);
+            return Ok("Deleted");
+        }
+    }
+}
