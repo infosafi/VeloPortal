04eb189e97e3ec057b1368af5bb20a52a4902def
rakib.khan <rakib.khan@ITVLAP-0009>
2026-02-03 11:46:44 +0600

    merged code

--- VeloPortal.Infrastructure/Data/Repositories/SystemConfig/ResCodeInfRepository.cs
+++ VeloPortal.Infrastructure/Data/Repositories/SystemConfig/ResCodeInfRepository.cs
@@ -3,6 +3,7 @@ using AutoMapper.QueryableExtensions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Logging;
+using System.Data;
 using VeloPortal.Application.DTOs.SystemConfig;
 using VeloPortal.Application.Interfaces.SystemConfig;
 using VeloPortal.Application.Settings;
@@ -84,6 +85,35 @@ namespace VeloPortal.Infrastructure.Data.Repositories.SystemConfig
                 ErrorTrackingExtension.SetError(ex);
                 _logger.LogError(ex, "Accounts Code Retrival Failed");
                 return null;
+            }
+        }
+
+        public async Task<IEnumerable<dynamic>?> GetUserWiseResCodeBookInfo(string? comcod, string? label, string? search_query, int user_id, bool? is_manual_post, bool? is_last_head)
+        {
+            try
+            {
+                await Task.Delay(1);
+                if (_spProcessAccess == null)
+                {
+                    _logger.LogWarning("_spProcessAccess is not initialized.");
+                    return await Task.FromResult<IEnumerable<dynamic>?>(null);
+                }
+                IEnumerable<dynamic>? lst = null;
+                DataSet? ds = _spProcessAccess.GetTransInfo20(comcod ?? "", "itv_sys.SP_CODEBOOK_MGT", "Get_Res_Code_Book_Information", label ?? string.Empty,
+                   search_query?.ToString() ?? string.Empty, user_id.ToString(), is_manual_post.ToString() ?? "", is_last_head.ToString() ?? "");
+                if (ds == null || ds.Tables.Count == 0 || ds.Tables[0].Rows.Count == 0)
+                {
+                    return await Task.FromResult<IEnumerable<dynamic>?>(null);
+                }
+                lst = ds.Tables[0].DataTableToDynamicList();
+                return await Task.FromResult<IEnumerable<dynamic>?>(lst);
+            }
+            catch (Exception ex)
+            {
+                ErrorTrackingExtension.SetError(ex);
+                _logger.LogError(ex, "Res Code Book Retrival Failed");
+
+                return await Task.FromResult<IEnumerable<dynamic>?>(null);
             }
         }
 
--- VeloPortal.WebApi/Controllers/V1/SystemConfig/CodeBookController.cs
+++ VeloPortal.WebApi/Controllers/V1/SystemConfig/CodeBookController.cs
@@ -54,5 +54,46 @@ namespace VeloPortal.WebApi.Controllers.V1.SystemConfig
         }
 
         #endregion
+
+        #region Dynamic Code Book
+
+        /// <summary>
+        /// Gets List of Resources Code Book
+        /// </summary>
+        /// <param name="comcod">This the Company Code, such like 11001.</param>
+        /// <param name="label">This the Resources label. such like 2,4,7,9,12.</param>
+        /// <param name="search_query">This the Dyanmic Query Resources code, Example rescode like '%01%'.</param>       
+        /// <param name="user_id">This the Current user, if it set code book retrive as user permission if applicable.</param>        
+        /// <param name="is_manual_post">This the Boolean Paramter indicate Chatered Accounts code is for Manual Posting?, Default is false.</param>
+        /// <param name="is_last_head">This the Boolean Paramter indicate Last Transection or not default all are retrive.</param>
+
+        /// <returns>List of Resources Code Book</returns>
+        [HttpGet("get-dynamic-res-code")]
+        public async Task<IActionResult> GetResourcesCodeBookInformation(string? comcod, string? label, string? search_query, int user_id, bool? is_manual_post,
+             bool is_last_head = true)
+        {
+            if (label == null || label.Length == 0 || Convert.ToInt32(label) > 12)
+            {
+                return NotFound(ApiResponse<DtoResCodeInf>.FailureResponse(
+                    new List<string> { "Label is Mandatory or Not Appropriate" }, ErrorTrackingExtension.ErrorMsg ?? "Error Occured"));
+            }
+            if (search_query == null || search_query.Length == 0)
+            {
+                return NotFound(ApiResponse<DtoResCodeInf>.FailureResponse(
+                    new List<string> { "Searching query needed" }, ErrorTrackingExtension.ErrorMsg ?? "Error Occured"));
+            }
+            var response = await _rescodeInf.GetUserWiseResCodeBookInfo(comcod, label, search_query, user_id, is_manual_post, is_last_head);
+
+
+            if (response == null)
+                return NotFound(ApiResponse<IEnumerable<DtoResCodeInf>>.FailureResponse(
+                    new List<string> { "Resources  Code not found or Query is wrong" }, ErrorTrackingExtension.ErrorMsg ?? "Error Occured"));
+
+            if (response.Count() == 0)
+                return NotFound(ApiResponse<IEnumerable<dynamic>>.SuccessResponse(response, message: "No Data Found"));
+
+            return Ok(ApiResponse<IEnumerable<dynamic>>.SuccessResponse(response, message: ""));
+        }
+        #endregion
     }
 }
