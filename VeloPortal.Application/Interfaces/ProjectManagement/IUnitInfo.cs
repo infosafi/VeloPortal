4e6dd2fdd823b993438f5a03db3fa036cbc0a064
rakib.khan <rakib.khan@ITVLAP-0009>
2026-02-17 16:12:33 +0600

    new code changes

---
+++ VeloPortal.Application/Interfaces/ProjectManagement/IUnitInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace VeloPortal.Application.Interfaces.ProjectManagement
+{
+    public interface IUnitInfo
+    {
+        Task<IEnumerable<dynamic>?> GetUnitInfoByCodeAsync(string? comcod, string? acccode, string? unitType = "%", string? floor = "%");
+    }
+}
